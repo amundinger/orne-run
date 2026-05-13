6e47ecb2451e45e37f992983449003c44abc5b82
Arne Mundinger <amundinger@testo.de>
2026-05-13 22:58:55 +0200

    remove footer entries

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,33 +8,6 @@ export default function Footer({ setActiveTab }) {
           </a>
           <p>Der Sportivo Orne Lauf verbindet Sport, Natur und Gemeinschaft im malerischen Langenordnach bei Titisee-Neustadt im Schwarzwald.</p>
         </div>
-        <div className="footer-col">
-          <h5>Navigation</h5>
-          <ul>
-            <li><a onClick={() => setActiveTab('home')}>Home</a></li>
-            <li><a onClick={() => setActiveTab('anmeldung')}>Anmeldung</a></li>
-            <li><a onClick={() => setActiveTab('strecken')}>Strecken</a></li>
-            <li><a onClick={() => setActiveTab('sponsoren')}>Sponsoren</a></li>
-          </ul>
-        </div>
-        <div className="footer-col">
-          <h5>Information</h5>
-          <ul>
-            <li><a href="#">Anreise &amp; Parken</a></li>
-            <li><a href="#">Ergebnisse</a></li>
-            <li><a href="#">Kontakt</a></li>
-            <li><a href="#">Presse</a></li>
-          </ul>
-        </div>
-        <div className="footer-col">
-          <h5>Rechtliches</h5>
-          <ul>
-            <li><a href="#">Impressum</a></li>
-            <li><a href="#">Datenschutz</a></li>
-            <li><a href="#">AGB</a></li>
-            <li><a href="#">Teilnahmebedingungen</a></li>
-          </ul>
-        </div>
       </div>
       <div className="footer-bottom">
         <span>© 2026 Sportivo Orne Lauf e.V. – Alle Rechte vorbehalten</span>
